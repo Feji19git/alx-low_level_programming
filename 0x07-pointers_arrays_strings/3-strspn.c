c506fb449846a1ca8745adff2a834671cdc5a549
Feji19git <charlesoghenefejiro19@gmail.com>
2022-09-26 16:44:58 -0700

    task*

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,19 +8,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int count = 0, supposed_count = 0;
-  int i, j;
+	unsigned int count = 0, supposed_count = 0;
+	int i, j;
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (j = 0; accept[j] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	  if (s[i] == accept[j])
-	    count++;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				count++;
+		}
+		supposed_count++;
+		if (count == 0 || count != supposed_count)
+			break;
 	}
-      supposed_count++;
-      if (count == 0 || count != supposed_count)
-	break;
-    }
-  return (count);
+	return (count);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,14 +9,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-  int i, j;
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (j = 0; accept[j] != '\0'; j++)
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	  if (s[i] == accept[j])
-	    return (&s[i]);
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
 	}
-    }
-  return (NULL);
+	return (NULL);
 }
