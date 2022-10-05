65edf1ca469ab1460d3b4da72c7ab38db19abd45
Feji19git <charlesoghenefejiro19@gmail.com>
2022-10-04 19:52:34 -0700

    free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 char **strtow(char *);
 char *argstostr(int, char **);
 void free_grid(int **grid, int row);
-int **alloc_grid(int col, int row);
+int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
